0b2b78ab8e1e2af5c75e1d63fd725e197bc8db7f
SUELEN POGALSKI <suelen.pogalski@unidavi.edu.br>
2022-10-04 16:10:23 -0300

    add text

--- resources/views/saude.blade.php
+++ resources/views/saude.blade.php
@@ -4,5 +4,6 @@
     <p>Possibilita analisar e calcular indicadores de saúde de uma pessoa. <br />Como seu IMC e qualidade do sono.</p>
 @endsection
 @section('content')
-   <p>Escolha uma opção.</p>
+   <p>Escolha uma opção acima!<br>
+            (barra)</p>
 @endsection
